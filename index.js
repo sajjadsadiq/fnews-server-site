ef3567973f047aafb08f1c52b6bc9631c7b377a8
sajjad sadiq <sajjadsadiq2020@gmail.com>
2021-09-20 20:23:05 +0600

    update post, user, auth

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const authRoutes = require('./Routes/Auth')
+const addPosts = require('./Routes/Post')
 
-dotenv.config()
 
-app.use("/saj", (req, res)=>{
-    console.log("Hello respone nodemon backend data! facebook page: sajjad sadiq");
+dotenv.config();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL,{
 })
+.then(console.log("connneted to MongoDB"))
+.catch(error => console.log(error));
+
+app.use("/auth", authRoutes)
+app.use("/posts", addPosts)
 
 app.listen("5000", ()=>{
     console.log("backend is running");
